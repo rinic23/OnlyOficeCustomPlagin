47fcb91f05907d6cf251f3e8d78fdf9ef2318229
Yerichev Yegor <yegor.yerichev@nexign.com>
2023-10-08 12:48:49 +0300

    First commit

---
+++ scripts/code.js
@@ -0,0 +1,39 @@
+(function(window, undefined){
+
+    var isInit = false;
+    window.Add = function(field_type)
+    {
+        if (!isInit)
+            return;
+
+        // serialize command as text
+        var sScript = "var oDocument = Api.GetDocument();";
+        sScript += "var oParagraph = Api.CreateParagraph();";
+        sScript += "oParagraph.AddText(\'" + field_type + "\');";
+        sScript += "oDocument.InsertContent([oParagraph]);";
+        window.Asc.plugin.info.recalculate = true;
+        window.Asc.plugin.executeCommand("command", sScript);
+    };
+    window.Mark = function()
+    {
+        return window.Add('${DynamicTable}');
+    };
+
+    window.Asc.plugin.init = function()
+    {
+        isInit = true;
+    };
+
+    window.Asc.plugin.button = function(id)
+    {
+        if (-1 == id)
+            this.executeCommand("close", "");
+    };
+
+})(window, undefined);
+
+console.log('hi')
+
+fetch('https://jsonplaceholder.typicode.com/posts/1')
+    .then((response) => response.json())
+    .then((json) => console.log(json));
